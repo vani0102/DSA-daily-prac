20b94616542fe9e5c3e75df720c68d5e71a993a7
vani0102 <vani1204sharma@gmail.com>
2026-05-07 02:25:54 +0530

    Implement left rotation of array by one

---
+++ Arrays/Left Rotate array by one.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int temp = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+            nums[i - 1] = nums[i];
+        }
+
+        nums[nums.size() - 1] = temp;
+    }
+};
